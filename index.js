d1a05e0753ecd46301f820e4242ed01f721a0516
Gabriel Velloso <161901201+iVelloso@users.noreply.github.com>
2024-04-06 14:01:51 -0300

    Add files via upload
    index.js / styles.js -> AMBOS DA PASTA HOME

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
-import { Text, View } from "react-native"
+import { View, Text } from "react-native";
+import { TouchableOpacity } from "react-native-web";
 
-export default function Cadastro(){
+export default function Home({ navigation }){
+    
+    function acessarCadastro(){
+        navigation.navigate('Cadastro');
+
+    }
     return(
         <View>
-
             <Text>
-                -- ESTOU NO CADASTRO --
+                -- ESTOU NA HOME --
             </Text>
-        
+            <TouchableOpacity
+                onPress={acessarCadastro}
+                >
+                <Text>
+                    Acessar Cadastro
+                </Text>
+            </TouchableOpacity>
         </View>
     );
 }
