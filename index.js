1da7a288b8b36bf7c889472034cf9d454557af72
Gabriel Velloso <161901201+iVelloso@users.noreply.github.com>
2024-04-06 14:00:55 -0300

    Add files via upload
    index.js / styles.js -> AMBOS DA PASTA CADASTRO

---
+++ index.js
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native"
+
+export default function Cadastro(){
+    return(
+        <View>
+
+            <Text>
+                -- ESTOU NO CADASTRO --
+            </Text>
+        
+        </View>
+    );
+}
